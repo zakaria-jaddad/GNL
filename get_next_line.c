5ad54dcfa3dd63b40caa00569406b2200c440317
Zakaria Jaddad <zajaddad@e2r5p7.1337.ma>
2024-11-09 12:35:08 +0100

    gnl i wich i knew what i'm doing

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,18 @@
 /*   By: zajaddad <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 12:45:02 by zajaddad          #+#    #+#             */
-/*   Updated: 2024/11/07 11:08:15 by zajaddad         ###   ########.fr       */
+/*   Updated: 2024/11/09 12:32:57 by zajaddad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/fcntl.h>
 #include <unistd.h>
 
-int	check_new_line(char *s)
+
+int get_new_line_index(char *s)
 {
 	int	i;
 	if (s == NULL)
@@ -23,66 +25,70 @@ int	check_new_line(char *s)
 	i = 0;
 	while (s[i])
 	{
-		if (s[i] == '\n')
+		if (s[i++] == '\n')	
 			return (i);
-		i++;
 	}
 	return (0);
 }
 
-char	*read_line(int fd, char *result)
-{
-	int	n_bytes;
-	int	i;
-	char	*content;
 
-	content = NULL;
-	if (result && check_new_line(result))
-		return (result);
+char *read_line(int fd, char *result)
+{
+	char	*buffer;
+	char	*content = ft_strdup(result);
+	int n_bytes;
+	if (content && get_new_line_index(content))
+		return (content);
 	while (1)
 	{
-		content = (char *) malloc(BUFFER_SIZE + 1);
-		if (content == NULL)
-			break ;
-		if ((n_bytes = read(fd, content, BUFFER_SIZE)) <= 0)
-			break ;
-		result = ft_strjoin(result, content);
-		free(content);
-		if (result == NULL || check_new_line(result))
+		buffer = (char *) ft_calloc(sizeof(char), (size_t) (BUFFER_SIZE + 1));
+		if (buffer == NULL)
+		{
+			free(content);
+			return (NULL);
+		}
+		if ((n_bytes = read(fd, buffer, BUFFER_SIZE)) <= 0) // 0 if read nothing -1 if EOF
+		{
+			free(buffer);
+			return (NULL);
+		}
+		content = ft_strjoin(content, buffer);
+		free(buffer);
+		if (content == NULL || get_new_line_index(content))
 			break ;
 	}
-	return (result);
-}
-
-char	*get_next_line(int fd)
-{
-	char *content;
-	static char	*rest;
-	int		i;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	content = read_line(fd, rest);
-	if (content == NULL)
-		return (NULL);
-	if ((i = check_new_line(content)))
-	{
-		rest = ft_strdup(content + i + 1);
-		content = ft_substr(content, 0, i + 1);
-		return (content);
-	}
-	rest = NULL;
 	return (content);
 }
 
-int main(void) {
-	int fd = open("file.txt", O_RDONLY, 0);
-	char *line;
+char *get_next_line(int fd)
+{
+	static char	*rest;
+	char		*buffer;
+	char		*line;
+	int	i;
 
-	while ((line = get_next_line(fd)))
-	{
-		printf("%s", line);
-	}
-	return EXIT_SUCCESS;
+	i = 0;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buffer = read_line(fd, rest);
+	if (buffer == NULL)
+		return (NULL);
+	// here is the limbo;
+	i = get_new_line_index(buffer);
+	rest = buffer + i;
+	line = ft_substr(buffer, 0, i);
+	free(buffer);
+	return (line);	
 }
 
+/* int main(int argc, char *argv[]) */
+/* { */
+/* 	int fd = open("file.txt", O_RDONLY, 0); */
+/* 	char *line; */
+/* 	while ((line = get_next_line(fd))) */
+/* 	{ */
+/* 		printf("%s", line); */
+/* 		free(line); */
+/* 	} */
+/* 	return EXIT_SUCCESS; */
+/* } */
